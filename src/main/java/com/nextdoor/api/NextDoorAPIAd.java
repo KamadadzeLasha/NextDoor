76ad2eaf3bf1ee007ffd65b3a340852f392c450e
Nika Beridze <rexoman65@gmail.com>
2024-09-23 17:55:40 +0400

    add: ad update; ad status update

--- src/main/java/com/nextdoor/api/NextDoorAPIAd.java
+++ src/main/java/com/nextdoor/api/NextDoorAPIAd.java
@@ -25,6 +25,10 @@ public class NextDoorAPIAd extends NextDoorAPIRequestNode {
         return new NextDoorAPICreateAd(this.nextDoorAPIAuth);
     }
 
+    public NextDoorAPIUpdateAd updateAd() {
+        return new NextDoorAPIUpdateAd(this.nextDoorAPIAuth);
+    }
+
     public static class NextDoorAPICreateAd extends NextDoorAPIRequest<Ad> implements NextDoorAPIExecute<Ad> {
         public NextDoorAPICreateAd(NextDoorAPIAuth nextDoorAPIAuth) {
             super(Ad.class, nextDoorAPIAuth);
@@ -97,6 +101,154 @@ public class NextDoorAPIAd extends NextDoorAPIRequestNode {
             }
 
             public AdCreateException(String s, Throwable throwable, boolean b, boolean b1) {
+                super(s, throwable, b, b1);
+            }
+        }
+    }
+
+    public static class NextDoorAPIUpdateAd extends NextDoorAPIRequest<Ad> implements NextDoorAPIExecute<Ad> {
+        public NextDoorAPIUpdateAd(NextDoorAPIAuth nextDoorAPIAuth) {
+            super(Ad.class, nextDoorAPIAuth);
+
+            this.addHeader(this.getNextDoorAPIAuth().getTokenHeader());
+        }
+
+        public NextDoorAPIUpdateAd setId(String id) {
+            this.setParamInternal("id", id);
+
+            return this;
+        }
+
+        public NextDoorAPIUpdateAd setAdvertiserId(String advertiserId) {
+            this.setParamInternal("advertiser_id", advertiserId);
+
+            return this;
+        }
+
+        public NextDoorAPIUpdateAd setName(String name) {
+            this.setParamInternal("name", name);
+
+            return this;
+        }
+
+        public NextDoorAPIUpdateAd setCreativeId(String creativeId) {
+            this.setParamInternal("creative_id", creativeId);
+
+            return this;
+        }
+
+        @Override
+        protected String getPath() {
+            return DEFAULT_FULL_ADS_API_URL + "ad/update";
+        }
+
+        @Override
+        protected void validateRequiredParams() {
+
+        }
+
+        @Override
+        public Ad execute() throws APIRequestException {
+            validateRequiredParams();
+
+            try {
+                return sendHttpRequest(HttpMethod.POST, ConversionType.JSON);
+            } catch (APIRequestException e) {
+                throw new AdUpdateException("Can't update Ad, because of: " + e.getLocalizedMessage());
+            }
+        }
+
+        public static class AdUpdateException extends APIRequestException {
+            public AdUpdateException() {
+            }
+
+            public AdUpdateException(String s) {
+                super(s);
+            }
+
+            public AdUpdateException(String s, Throwable throwable) {
+                super(s, throwable);
+            }
+
+            public AdUpdateException(Throwable throwable) {
+                super(throwable);
+            }
+
+            public AdUpdateException(String s, Throwable throwable, boolean b, boolean b1) {
+                super(s, throwable, b, b1);
+            }
+        }
+    }
+
+    public static class NextDoorAPIUpdateAdStatus extends NextDoorAPIRequest<Ad> implements NextDoorAPIExecute<Ad> {
+        public NextDoorAPIUpdateAdStatus(NextDoorAPIAuth nextDoorAPIAuth) {
+            super(Ad.class, nextDoorAPIAuth);
+
+            this.addHeader(this.getNextDoorAPIAuth().getTokenHeader());
+        }
+
+        public NextDoorAPIUpdateAdStatus setId(String id) {
+            this.setParamInternal("id", id);
+
+            return this;
+        }
+
+        public NextDoorAPIUpdateAdStatus setAdvertiserId(String advertiserId) {
+            this.setParamInternal("advertiser_id", advertiserId);
+
+            return this;
+        }
+
+        public NextDoorAPIUpdateAdStatus setUserStatus(UserStatus userStatus) {
+            this.setParamInternal("user_status", userStatus);
+
+            return this;
+        }
+
+        @Override
+        protected String getPath() {
+            return DEFAULT_FULL_ADS_API_URL + "ad/status/update";
+        }
+
+        @Override
+        protected void validateRequiredParams() {
+
+        }
+
+        @Override
+        public Ad execute() throws APIRequestException {
+            validateRequiredParams();
+
+            try {
+                return sendHttpRequest(HttpMethod.POST, ConversionType.JSON);
+            } catch (APIRequestException e) {
+                throw new AdStatusUpdateException("Can't update Ad status, because of: " + e.getLocalizedMessage());
+            }
+        }
+
+        public enum UserStatus {
+            ACTIVE,
+            INACTIVE,
+            ARCHIVED
+        }
+
+        public static class AdStatusUpdateException extends APIRequestException {
+            public AdStatusUpdateException() {
+            }
+
+            public AdStatusUpdateException(String s) {
+                super(s);
+            }
+
+            public AdStatusUpdateException(String s, Throwable throwable) {
+                super(s, throwable);
+            }
+
+            public AdStatusUpdateException(Throwable throwable) {
+                super(throwable);
+            }
+
+            public AdStatusUpdateException(String s, Throwable throwable, boolean b, boolean b1) {
                 super(s, throwable, b, b1);
             }
         }
